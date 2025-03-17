a430e986957cc7145490bbaa7ccf218c96ce5729
JakobSkoglund <skoglund1@outlook.com>
2025-03-17 23:06:20 +0100

    More structure to the proejct and planning for future coding

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 //import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import  Home  from "./Pages/Home";
+import  Home  from "./Pages/View/Home";
 import  About  from "./Pages/About"; 
 import  Footer  from "./components/Footer";
 import  Navbar from "./components/Navbar"; 
 import './App.css'
 
 function App() {
-
-
   return (
     <Router>
       <Navbar />
